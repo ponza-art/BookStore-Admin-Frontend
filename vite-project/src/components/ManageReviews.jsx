c97ba2501873f4e6ea3cffad690423a480343034
MKHNENE1 <mohamedhalabya11@gmail.com>
2024-10-26 11:25:05 +0300

    update reviews page

--- vite-project/src/components/ManageReviews.jsx
+++ vite-project/src/components/ManageReviews.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import { getAllReviews, deleteReview } from "../services/api";
 import Swal from "sweetalert2";
+import { FaTrash } from "react-icons/fa";
 
 const ReviewsPage = () => {
   const [reviews, setReviews] = useState([]);
@@ -25,21 +27,47 @@ const ReviewsPage = () => {
 
   const handleDelete = async (reviewId) => {
     const result = await Swal.fire({
-      title: "Are you sure?",
+      title: "Are you sure ?",
       text: "This action cannot be undone!",
       icon: "warning",
       showCancelButton: true,
       confirmButtonText: "Yes, delete it!",
       cancelButtonText: "No, cancel!",
+      confirmButtonColor: "#b90303",
+      cancelButtonColor: "#e2e8f0",
+      didOpen: () => {
+        const popup = Swal.getPopup();
+        if (popup) {
+          popup.style.borderRadius = "12px";
+          const cancelButton = popup.querySelector(".swal2-cancel");
+          if (cancelButton) cancelButton.style.color = "black";
+        }
+      },
     });
 
     if (result.isConfirmed) {
+      setLoading(true);
       try {
         await deleteReview(reviewId);
         setReviews(reviews.filter((review) => review._id !== reviewId));
-        Swal.fire("Deleted!", "The review has been deleted.", "success");
+        setLoading(false);
+        Swal.fire("Deleted!", "The review has been deleted.", "success", {
+          didOpen: () => {
+            const popup = Swal.getPopup();
+            if (popup) {
+              popup.style.borderRadius = "12px";
+            }
+          },
+        });
       } catch (err) {
-        Swal.fire("Error!", "Failed to delete review.", "error");
+        Swal.fire("Error!", "Failed to delete review.", "error", {
+          didOpen: () => {
+            const popup = Swal.getPopup();
+            if (popup) {
+              popup.style.borderRadius = "12px";
+            }
+          },
+        });
       }
     }
   };
@@ -50,53 +78,83 @@ const ReviewsPage = () => {
       review.bookId.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
-  if (loading) return <span>Loading...</span>;
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center">
+        <span
+          className="loading loading-bars loading-lg text-blue-800"
+          style={{ width: "20%", margin: "30vh 30vw" }}
+        ></span>
+      </div>
+    );
+  }
   if (error) return <p>{error}</p>;
 
   return (
-    <div className="p-6 bg-base-100 text-neutral font-sans">
-      {/* Title */}
-      <h1 className="text-3xl font-bold mb-4 text-primary-brown">Manage Reviews</h1>
+    <div className="py-6 mt-16 lg:mt-11 bg-base-100 text-neutral font-sans p-4 lg:p-8 flex flex-col items-center shadow-lg rounded-lg">
+      <div className="w-full">
+        <div className="flex flex-wrap justify-center gap-x-4 sm:justify-between">
+          <h1 className="text-3xl font-bold mb-4 text-primary-brown">
+            Manage Reviews
+          </h1>
+          <input
+            type="text"
+            placeholder="Search Reviews..."
+            className="input input-bordered mb-4 text-primary-blue"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
 
-      {/* Search Input */}
-      <input
-        type="text"
-        placeholder="Search Reviews..."
-        className="input input-bordered mb-4 text-primary-blue"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-
-      {/* Table */}
-      <table className="table-auto w-full text-left border border-neutral">
-        <thead className="bg-primary-brown text-white">
-          <tr>
-            <th className="p-2">Comment</th>
-            <th className="p-2">Rating</th>
-            <th className="p-2">Book Title</th>
-            <th className="p-2">User</th>
-            <th className="p-2">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredReviews.map((review) => (
-            <tr key={review._id} className="bg-white hover:bg-accent text-neutral">
-              <td className="p-2">{review.comment}</td>
-              <td className="p-2">{review.rating}</td>
-              <td className="p-2">{review.bookId.title}</td>
-              <td className="p-2">{review.userId.username}</td>
-              <td className="p-2">
-                <button
-                  className="btn btn-accent ml-2 text-base-100"
-                  onClick={() => handleDelete(review._id)}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+        <div className="overflow-x-auto w-full">
+          {/* className="table-auto table-pin-rows table-pin-cols border w-full text-center shadow-md rounded-lg mb-8" */}
+          <table className="table w-full text-center rounded-lg mb-3">
+            <thead>
+              <tr className="rounded-sm bg-[#f7f9fc] tab-border-2 text-sm">
+                <th className="px-4 py-2 text-start">Comment</th>
+                <th className="px-4 py-2 text-start">Rating</th>
+                <th className="px-4 py-2 text-start">Book Title</th>
+                <th className="px-4 py-2 text-start">User</th>
+                <th className="px-4 py-2">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredReviews.length > 0 ? (
+                filteredReviews.map((review) => (
+                  <tr
+                    key={review._id}
+                    className="border-t hover:bg-gray-100"
+                    // className="bg-white hover:bg-accent text-neutral"
+                  >
+                    <td className="px-4 py-2 text-start">{review.comment}</td>
+                    <td className="px-4 py-2 text-start">{review.rating}</td>
+                    <td className="px-4 py-2 text-start">
+                      {review.bookId.title}
+                    </td>
+                    <td className="px-4 py-2 text-start">
+                      {review.userId.username}
+                    </td>
+                    <td className="px-4 py-2">
+                      <button
+                        className="text-blue-800 duration-500 ml-2 hover:text-black p-2"
+                        onClick={() => handleDelete(review._id)}
+                      >
+                        <FaTrash size={16} />
+                      </button>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="5" className="text-center py-4">
+                    No reviews found
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
